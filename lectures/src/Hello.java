5a08769aa8af8c2cbbde1aec64b4838a46ffaec9
Tony Jenkins <tony@tony-jenkins.co.uk>
2023-10-09 12:58:21 +0100

    Add sample programs for Week 3 lecture

---
+++ lectures/src/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+
+    public Hello () {
+        sayHello();
+    }
+
+    public void sayHello () {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        new Hello();
+    }
+
+}

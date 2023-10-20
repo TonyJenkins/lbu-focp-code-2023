f6f7408a9c87f9fa4f8fa1e3246fb87c3d8baa59
Tony Jenkins <tony@tony-jenkins.co.uk>
2023-10-20 11:31:28 +0100

    Fix formative test Task 2

--- lectures/04/Task2/hello_name.py
+++ lectures/04/Task2/hello_name.py
@@ -3,7 +3,7 @@
 if __name__ == '__main__':
     name = input('Hello! What is your name? ')
 
-    if len(name) > 0:
+    if name:
         print(f'Well met, {name}! It is good to meet you!')
     else:
         print('Well met, Mysterious Stranger! How shall we learn your name?')

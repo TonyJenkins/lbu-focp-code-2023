1ac6c8e1b18aad3516f5fd4110b6c65e0256bae4
p-i-999983 <65596849+p-i-999983@users.noreply.github.com>
2023-11-07 16:10:25 +0000

    Create total_calculator.py

---
+++ practicals/fs/group_a/Week07/total_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# this takes a file (CLA) and adds all numbers together and appends the file with the Total. 
+
+import sys
+
+
+if __name__ == '__main__':
+
+    try:
+        with open(sys.argv[1], 'r+') as f:
+            print('File found.')
+            total = 0
+            for line in f:
+                try:
+                    total += int(line)
+                except ValueError:
+                    print("found dodgy data")
+            print(total)
+            f.write("Total is " + str(total))
+    except FileNotFoundError:
+        print('File not found')
+    except IndexError:
+        print(f'{sys.argv[0]}: Missing CLA')

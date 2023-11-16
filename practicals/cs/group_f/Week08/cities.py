92c4f1f20a04af738cd08f829c2527fcb52a8b41
Patrick <p@p>
2023-11-16 14:33:20 +0000

    Patrick's files used in session for CSF Week 8

---
+++ practicals/cs/group_f/Week08/cities.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+#Thank you Tony for the great example!
+
+def chomp(s):
+    return s[:-1]
+
+
+def read_city_file(filename='cities.txt'):
+
+    cities = {}
+
+    with open(filename, 'r') as c:
+        for line in c:
+            country, capital = line.split(',')
+            cities[country] = chomp(capital)
+
+    return cities
+
+
+def write_city_file(cities, filename='cities.txt'):
+    with open(filename, 'w') as c:
+        for country in cities:
+            c.write(f'{country},{cities[country]}\n')
+
+
+if __name__ == '__main__':
+
+    cities = read_city_file()
+    print(cities)
+
+    country = input('Enter a country: ')
+    if country.lower() in cities:
+        print(f'The capital of {country.capitalize()} is {cities[country].capitalize()}.')
+    else:
+        capital = input('Enter the capital: ')
+        cities[country] = capital
+
+    print(cities)
+    write_city_file(cities)
